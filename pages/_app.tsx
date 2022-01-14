b80df47d7aa17e403d98124d6c657331f3ebe2a2
Gazmendrx Pabau <gazmend@pabau.com>
2022-01-14 22:52:28 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const spacexClient = new ApolloClient({
+  uri: ' https://api.spacex.land/graphql',
+  cache: new InMemoryCache()
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={ spacexClient } >
+      <Component {...pageProps} />
+    </ApolloProvider> 
+  )
 }
 
 export default MyApp
